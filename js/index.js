fa7090aa3867fd0874d8b2de1600ebca1661b72f
Midnights <hoangminhtan120399@gmail.com>
2021-07-10 18:33:37 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const APP_ID = 'f8c0327a1d48236ae5ba14fbe07fb86b';
+const DEFAULT_VALUE = '--'
+
+const searchInput = document.querySelector('#search-input');
+const cityName = document.querySelector('.city-name');
+const weatherSate = document.querySelector('.weather-state');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+
+const sunrise = document.querySelector('.sunrise');
+const sunset = document.querySelector('.sunset');
+const humidity = document.querySelector('.humidity');
+const windSpeed = document.querySelector('.wind-speed');
+
+searchInput.addEventListener('change', (e) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}&units=metric&lang=vi`)
+        .then(async res => {
+            const data = await res.json();
+            console.log('[Search Input', data);
+            cityName.innerHTML = data.name || DEFAULT_VALUE;
+            weatherSate.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+            weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+            temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
+            sunrise.innerHTML = moment.unix(data.sys.sunrise).format('H:mm') || DEFAULT_VALUE;
+            sunset.innerHTML = moment.unix(data.sys.sunset).format('H:mm') || DEFAULT_VALUE;
+            humidity.innerHTML = data.main.humidity || DEFAULT_VALUE;
+            windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE;
+        });
+});
